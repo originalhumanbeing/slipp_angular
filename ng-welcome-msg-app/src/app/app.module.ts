cbe2a73506693deee597d3e48241cb477bca9177
hyunjung.han <han.hyunjung17@gmail.com>
2017-07-17 02:02:42 +0900

    first angular application

---
+++ ng-welcome-msg-app/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { WelcomeMsgComponent } from './welcome-msg/welcome-msg.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WelcomeMsgComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
